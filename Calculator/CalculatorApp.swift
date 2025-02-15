d13550f634a72b40931f8e0711df796f83069af2
Jean Carlo <jecachusa.1996@gmail.com>
2025-02-15 16:49:32 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Jean Carlo Chunga Sandoval on 15/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
